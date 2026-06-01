e5547d217e6d0a6ebec3b0f580ff5ab3822599ec
poluanya <borovaya.polinaaa06@gmail.com>
2026-06-01 15:16:39 +0300

    fix: исправила локаторы в CheckoutPage

--- pages/CheckoutPage.ts
+++ pages/CheckoutPage.ts
@@ -5,15 +5,15 @@ export class CheckoutPage {
   constructor(private readonly page: Page) {}
 
   async fillRandomForm() {
-    await this.page.getByTestId('firstName').fill(faker.person.firstName());
-    await this.page.getByTestId('lastName').fill(faker.person.lastName());
-    await this.page.getByTestId('postalCode').fill(faker.location.zipCode());
+    await this.page.getByTestId('first-name').fill(faker.person.firstName());
+    await this.page.getByTestId('last-name').fill(faker.person.lastName());
+    await this.page.getByTestId('postal-code').fill(faker.location.zipCode());
   }
 
   async fillForm(firstName: string, lastName: string, zipCode: string) {
-    await this.page.getByTestId('firstName').fill(firstName);
-    await this.page.getByTestId('lastName').fill(lastName);
-    await this.page.getByTestId('postalCode').fill(zipCode);
+    await this.page.getByTestId('first-name').fill(firstName);
+    await this.page.getByTestId('last-name').fill(lastName);
+    await this.page.getByTestId('postal-code').fill(zipCode);
   }
 
   async continue() {
