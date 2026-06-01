3b0aafa3ef8ff56916d4373a29324eea14f8b8b6
poluanya <borovaya.polinaaa06@gmail.com>
2026-06-01 15:52:29 +0300

    fix: изменения локаторов

--- pages/CheckoutPage.ts
+++ pages/CheckoutPage.ts
@@ -5,16 +5,20 @@ export class CheckoutPage {
   constructor(private readonly page: Page) {}
 
   async fillRandomForm() {
-    await this.page.getByTestId('first-name').fill(faker.person.firstName());
-    await this.page.getByTestId('last-name').fill(faker.person.lastName());
-    await this.page.getByTestId('postal-code').fill(faker.location.zipCode());
+    await this.page.getByRole('textbox', { name: /first name/i }).fill(faker.person.firstName());
+    await this.page.getByRole('textbox', { name: /last name/i }).fill(faker.person.lastName());
+    await this.page
+      .getByRole('textbox', { name: /zip\/postal code/i })
+      .fill(faker.location.zipCode());
   }
 
   async fillForm(firstName: string, lastName: string, zipCode: string) {
-    await this.page.getByTestId('first-name').waitFor({ state: 'visible' });
-    await this.page.getByTestId('first-name').fill(firstName);
-    await this.page.getByTestId('last-name').fill(lastName);
-    await this.page.getByTestId('postal-code').fill(zipCode);
+    const firstNameField = this.page.getByRole('textbox', { name: /first name/i });
+
+    await firstNameField.waitFor({ state: 'visible' });
+    await firstNameField.fill(firstName);
+    await this.page.getByRole('textbox', { name: /last name/i }).fill(lastName);
+    await this.page.getByRole('textbox', { name: /zip\/postal code/i }).fill(zipCode);
   }
 
   async continue() {
